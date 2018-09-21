8b6e5739d9b1bde7a5e884efac9e2af1e2df4320
shilko2013 <shilko2013@gmail.com>
2018-09-21 13:26:19 +0300

    Version 0.3. gauss iteration start.

--- src/com/shilko/ru/gauss/GaussMatrix.java
+++ src/com/shilko/ru/gauss/GaussMatrix.java
@@ -2,11 +2,13 @@ package com.shilko.ru.gauss;
 
 import java.util.*;
 
+import static java.lang.Math.*;
+
 public class GaussMatrix {
     private double[][] matrix;
 
     public static void main(String... args) {
-        MatrixIO.printMatrix(MatrixIO.readMatrix());
+        MatrixIO.printMatrix(MatrixIO.readMatrix("matrix.txt").gauss());
     }
 
     public double[][] getMatrix() {
@@ -47,10 +49,66 @@ public class GaussMatrix {
                 if (random.nextBoolean())
                     matrix[i][j] *= -1;
             }
-            return this;
+        return this;
     }
 
-    public void gauss() {
+    public GaussMatrix gauss() { //changes matrix
 
+        var equalVars = new int[matrix.length];
+
+        for (int i = 0; i < matrix.length; ++i) //заполнение таблицы соответствия столбца переменной
+            equalVars[i] = i;
+
+        for (int iteration = 0; iteration < matrix.length - 1; ++iteration) {
+
+            int n = matrix.length - iteration;
+
+            double max = matrix[0][0];
+            int maxi = 0, maxj = 0;
+
+            for (int i = 0; i < n; ++i) { //поиск наибольшего по модулю коэффициента
+                for (int j = 0; j < n; ++j) {
+                    if (abs(matrix[i][j]) > abs(max)) {
+                        max = matrix[i][j];
+                        maxi = i;
+                        maxj = j;
+                    }
+                }
+            }
+
+            double[] koef = new double[n]; //нахождение коэффициентов для умножения строк
+            for (int i = 0; i < n; ++i)
+                koef[i] = -matrix[i][maxj] / max;
+
+            for (int i = 0; i < n; ++i) { //сложение строк
+                for (int j = 0; j < n + 1; ++j) {
+                    if (i == maxi)
+                        continue;
+                    matrix[i][j] += matrix[maxi][j] * koef[i];
+                }
+            }
+
+            for (int j = 0; j < n + 1; ++j) { //формирование треугольной матрицы
+                double temp = matrix[iteration][j];
+                matrix[iteration][j] = matrix[maxi][j];
+                matrix[maxi][j] = temp;
+            }
+
+            for (int i = 0; i < n; ++i) {
+                double temp = matrix[i][iteration];
+                matrix[i][iteration] = matrix[i][maxj];
+                matrix[i][maxj] = temp;
+            }
+
+            var temp = equalVars[iteration];
+            equalVars[iteration] = equalVars[maxj];
+            equalVars[maxj] = temp;
+
+            MatrixIO.printMatrix(this);
+            System.out.println();
+
+        }
+
+        return this;
     }
 }
